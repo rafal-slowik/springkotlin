9db09c2ce7b7cc466ea5e3a0022372c8e4b91219
rslowik <rafal2308@gmail.com>
2023-11-12 21:04:39 +0100

    add logger

--- src/main/kotlin/com/rslowik/springkotlin/controller/UserController.kt
+++ src/main/kotlin/com/rslowik/springkotlin/controller/UserController.kt
@@ -2,6 +2,8 @@ package com.rslowik.springkotlin.controller
 
 import com.rslowik.springkotlin.dto.UserDto
 import com.rslowik.springkotlin.service.UserService
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
@@ -17,8 +19,11 @@ import java.util.UUID
 @RequestMapping("/api/v1/users")
 class UserController constructor(val userService: UserService) {
 
+    private val log = LoggerFactory.getLogger(this.javaClass)
+
     @GetMapping("/{id}")
     fun get(@Validated @PathVariable("id") userId: UUID): ResponseEntity<UserDto> {
+        log.info("Get request in progress ...")
         return ResponseEntity.ok(userService.get(userId))
     }
 }
