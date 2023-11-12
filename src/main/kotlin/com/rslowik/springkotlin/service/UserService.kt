ab0a696439af08469f40f8d6a86a41a91076f516
rslowik <rafal2308@gmail.com>
2023-11-12 22:40:03 +0100

    add logger

--- src/main/kotlin/com/rslowik/springkotlin/service/UserService.kt
+++ src/main/kotlin/com/rslowik/springkotlin/service/UserService.kt
@@ -1,12 +1,16 @@
 package com.rslowik.springkotlin.service
 
 import com.rslowik.springkotlin.dto.UserDto
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import java.util.UUID
 
 @Service
 class UserService {
+
+    private val log = LoggerFactory.getLogger(this.javaClass)
     fun get(userId: UUID): UserDto {
+        log.info("User ID {} will be returned", userId)
         return UserDto("Jimmy", "Lenny", UUID.randomUUID().toString())
     }
 }
